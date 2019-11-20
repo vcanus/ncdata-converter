773c3f75ad611ee2953e8b139cc1eb139660d2ea
yrShin <yrshin@vcanus.com>
2019-11-20 15:06:47 +0900

    push ncdata-converter with test(example)

---
+++ NcDataConverter/NcData.cs
@@ -0,0 +1,191 @@
+﻿using Newtonsoft.Json.Linq;
+using Rhino.Geometry;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace NC_Data
+{
+    public class NcData
+    {
+        private const string OPERATION = "operation";
+        private const string BOUNDINGBOX = "boundingBox";
+        private const string PARAMETERARRAY = "parameterArray";
+        private const string VALUEARRAY = "valueArray";
+        private Operation_t operation;
+        private BoundingBox_t boundingBox;
+        private ParameterArray_t parameterArray;
+        private ValueArray_t valueArray;
+
+        public Operation_t Operation { get => operation; set => operation = value; }
+        public BoundingBox_t BoundingBox { get => boundingBox; set => boundingBox = value; }
+        public ParameterArray_t ParameterArray { get => parameterArray; set => parameterArray = value; }
+        public ValueArray_t ValueArray { get => valueArray; set => valueArray = value; }
+        public NcData()
+        {
+            Operation = new Operation_t();
+            BoundingBox = new BoundingBox_t();
+            ParameterArray = new ParameterArray_t();
+            ValueArray = new ValueArray_t();
+        }
+
+        public JObject ToJson(string filename)
+        {
+            JObject jObject = new JObject();
+            jObject.Add(OPERATION, Operation.ToJson());
+            jObject.Add(BOUNDINGBOX, BoundingBox.ToJson());
+            jObject.Add(PARAMETERARRAY, ParameterArray.ToJson());
+            jObject.Add(VALUEARRAY, ValueArray.ToJson());
+            WriteToText(jObject.ToString(), filename);
+            return jObject;
+        }
+
+        private void WriteToText(String json, String filePath)
+        {
+            //현재 경로 설정은 임시
+            String currentPath = Environment.CurrentDirectory;
+            String fileName = "jsonFormat.txt";
+            filePath = currentPath + "\\" + fileName;
+            //
+
+            File.WriteAllText(filePath, json, Encoding.Default);
+        }
+
+        public class Operation_t
+        {
+            private Dictionary<string, object> operation;
+            public Dictionary<string, object> Operation { get => operation; set => operation = value; }
+
+            public readonly string type = "type";
+            public readonly string toolNo = "toolNo";
+            public readonly string startMapID = "startMapID";
+            public readonly string ncProgramFile = "ncProgramFile";
+            public readonly string stepover = "stepover";
+
+            public Operation_t() {
+                Operation = new Dictionary<string, object>();
+            }
+
+            public void Add(string key, object value)
+            {
+                Operation.Add(key, value);
+            }
+
+            public JObject ToJson()
+            {
+                JObject json = JObject.FromObject(Operation);
+                return json;
+            }
+        }
+
+        public class BoundingBox_t
+        {
+            private Point3d minPoint3d;
+            private Point3d maxPoint3d;
+
+            private const string MINPOINT = "minPoint";
+            private const string MAXPOINT = "maxPoint";
+            private const string x = "x";
+            private const string y = "y";
+            private const string z = "z";
+
+            public Point3d MinPoint { get => minPoint3d; set => minPoint3d = value; }
+            public Point3d MaxPoint { get => maxPoint3d; set => maxPoint3d = value; }
+
+            public BoundingBox_t() { }
+
+            public void SetMinPoint(double minX, double minY, double minZ)
+            {
+                MinPoint = new Point3d(minX, minY, minZ);
+            }
+
+            public void SetMaxPoint(double maxX, double maxY, double maxZ)
+            {
+                MaxPoint = new Point3d(maxX, maxY, maxZ);
+            }
+
+            public JObject ToJson()
+            {
+                JObject json = new JObject();
+                //JObject minJObject = JObject.FromObject(MinPoint);
+                JObject minJObject = new JObject();
+                minJObject.Add(x, MinPoint.X);
+                minJObject.Add(y, MinPoint.Y);
+                minJObject.Add(z, MinPoint.Z);
+
+                //JObject maxJObject = JObject.FromObject(MaxPoint);
+                JObject maxJObject = new JObject();
+                maxJObject.Add(x, MaxPoint.X);
+                maxJObject.Add(y, MaxPoint.Y);
+                maxJObject.Add(z, MaxPoint.Z);
+
+                json.Add(MINPOINT, minJObject);
+                json.Add(MAXPOINT, maxJObject);
+                return json;
+            }
+        }
+
+        public class ParameterArray_t
+        {
+            private List<string> parameterArray;
+
+            public List<string> ParameterArray { get => parameterArray; set => parameterArray = value; }
+
+            public readonly string x = "x";
+            public readonly string y = "y";
+            public readonly string z = "z";
+            public readonly string mMRROptiConverted = "mMRROptiConverted";
+            public readonly string mMRROrigConverted = "mMRROrigConverted";
+            public readonly string MovingDistance = "MovingDistance";
+
+            public ParameterArray_t()
+            {
+                ParameterArray = new List<string>();
+            }
+
+            public void Add(string param)
+            {
+                ParameterArray.Add(param);
+            }
+
+            public JArray ToJson()
+            {
+                JArray jArray = new JArray();
+                jArray = JArray.FromObject(ParameterArray);
+                return jArray;
+            }
+        }
+
+
+        public class ValueArray_t
+        {
+            private List<List<double>> valueArray;
+            
+            public List<List<double>> ValueArray { get => valueArray; set => valueArray = value; }
+
+            public ValueArray_t()
+            {
+                valueArray = new List<List<double>>();
+            }
+
+            public void AddList(List<double> list)
+            {
+                ValueArray.Add(list);
+            }
+
+            public void AddList(double[] arr)
+            {
+                AddList(arr.ToList());
+            }
+
+            public JArray ToJson()
+            {
+                JArray jArray = new JArray();
+                jArray = JArray.FromObject(ValueArray);
+                return jArray;
+            }
+        }
+    }
+}
---
+++ NcDataConverterTest/UnitTest1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NC_Data;
+using Newtonsoft.Json.Linq;
+
+namespace NcDataConverterTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private static NcData ncData;
+
+        public static NcData NcData { get => ncData; set => ncData = value; }
+
+        [AssemblyInitialize]
+        public static void Setting(TestContext testContext)
+        {
+            NcData = new NcData();
+            //Operation
+            NcData.Operation.Add(NcData.Operation.type, "Op01-001");
+            NcData.Operation.Add(NcData.Operation.toolNo, "Tool001");
+            NcData.Operation.Add(NcData.Operation.startMapID, "31145");
+            NcData.Operation.Add(NcData.Operation.ncProgramFile, "O0001.apt");
+            NcData.Operation.Add(NcData.Operation.stepover, 10.0);
+            //
+
+            //BoundingBox
+            NcData.BoundingBox.SetMinPoint(0.0, 0.0, 0.0);
+            NcData.BoundingBox.SetMaxPoint(100.0, 100.0, 100.0);
+            //
+
+            //ParameterArray
+            List<string> parameterList = new List<string>();
+            parameterList.Add(NcData.ParameterArray.x);
+            parameterList.Add(NcData.ParameterArray.y);
+            parameterList.Add(NcData.ParameterArray.z);
+            parameterList.Add(NcData.ParameterArray.mMRROptiConverted);
+            parameterList.Add(NcData.ParameterArray.mMRROrigConverted);
+            parameterList.Add(NcData.ParameterArray.MovingDistance);
+
+            foreach(string param in parameterList)
+            {
+                NcData.ParameterArray.Add(param);
+            }
+            //
+
+            //ValueArray
+            List<double> firstExampleList = new List<double>();
+            for(int i = 0; i < 10; i++)
+            {
+                firstExampleList.Add(i);
+            }
+
+            List<double> secondExampleList = new List<double>();
+            for(int i = 100; i < 110; i++)
+            {
+                secondExampleList.Add(i);
+            }
+
+            List<double> thirdExampleList = new List<double>();
+            for(int i = 200; i < 210; i++)
+            {
+                thirdExampleList.Add(i);
+            }
+
+            NcData.ValueArray.AddList(firstExampleList);
+            NcData.ValueArray.AddList(secondExampleList);
+            NcData.ValueArray.AddList(thirdExampleList);
+            //
+        }
+
+        [TestMethod]
+        public void TestNcData()
+        {
+            String directoryPath = Environment.CurrentDirectory;
+            String filePath = directoryPath + "\\jsonFormatTest.txt";
+            String original = File.ReadAllText(filePath);
+
+            //결과를 출력할 txt파일 이름을 변수로
+            JObject jObject = NcData.ToJson("jsonFormat.txt");
+            String test = jObject.ToString();
+            
+            Assert.AreEqual(original, test);
+        }
+
+        [AssemblyCleanup]
+        public static void CleanUp()
+        {
+            NcData = null;
+        }
+    }
+}
